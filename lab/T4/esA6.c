e6b676f2709d8fda381cc7423f6a52ad68305ed5
Luca <luca.modica@gmail.com>
2020-10-12 11:05:18 +0200

    Partial

--- lab/T4/esA6.c
+++ lab/T4/esA6.c
@@ -1,10 +1,43 @@
 #include <stdio.h>
 
+#define MAX_LENGTH 256
+
+int slength(char s[]);
+char *scut_last_word(char s[]);
+int string_is_empty(char s[]);
+
 
 int main(){
 
+    char str[MAX_LENGTH];
+
+    printf("Inserisci Stringa: ");
+    fgets(str,sizeof(str),stdin);
+
+    //Results
+    printf("\n1) Lunghezza della stringa: %d\n",slength(str));
+}
+
+
+int slength(char s[]){
+    int cont = 0;
+    for(char *c = s; *(c+1) != '\0'; c++){
+        cont++;
+    }
+    return cont;
+}
+
+char *scut_last_word(char s[]){
+
+}
+
+int string_is_empty(char s[]){
     
 }
+
+
+
+
 
 
 
--- lab/T4/esA7.c
+++ lab/T4/esA7.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
 
+#define MAX_LENGTH 256
+
+int slength(char s[]);
+char *scut_last_word(char s[]);
+int string_is_empty(char s[]);
+
 
 int main(){
 
+    char str[MAX_LENGTH];
+
+    printf("Inserisci Stringa: ");
+    scanf('%s',&str);
+
+    //Results
+    printf("\n1) Lunghezza della stringa: %d",slength(str));
+    printf("sfhdsihvidhdi");
+}
+
+
+int slength(char s[]){
+    int cont = 0;
+    for(char *c = s; *c != '\0'; c++){
+        cont++;
+    }
+    return cont;
+}
+
+char *scut_last_word(char s[]){
+
+}
+
+int string_is_empty(char s[]){
     
 }
 
