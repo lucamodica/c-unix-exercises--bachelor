9cc277393783b9517e59ed33988219614a852431
Luca <luca.modica@gmail.com>
2020-11-29 18:07:34 +0100

    partial es6 (handling pipe), L8

--- lab_Unix/L8/es_fifo_pipe/es5-writer.c
+++ lab_Unix/L8/es_fifo_pipe/es5-writer.c
@@ -3,12 +3,14 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <errno.h>
+#include <string.h>
 
 #define BUF_SIZE    1000
 
 
 int main(int argc, char * argv[]){
-  int fifo_fd, str_len;
+  int fifo_fd;
   char my_msg[BUF_SIZE];
   ssize_t num_bytes;
 
@@ -18,17 +20,22 @@ int main(int argc, char * argv[]){
   char *msgs[] = { "well cum", "getting cock", "dozzzer" };
 
   // Open FIFO in write mode
-  fifo_fd = open(argv[1], O_WRONLY);
+  while ((fifo_fd = open(argv[1], O_WRONLY)) == -1){
+	  fprintf(stderr,"%s: %d. Error in open() #%03d: %s",__FILE__,__LINE__,errno,strerror(errno));
+  }
+  
 
   //Writing the 3 messages
   for (int i = 0; i < 3; i++){
-	// Assemble the message
-	str_len = sprintf(my_msg,"%s\n",msgs[i]);
-	// Write message to FIFO
-	data_processed=write(fifo_fd, my_msg, str_len);
+	//Saving message on buffer
+	sprintf(my_msg,"%s\n",msgs[i]);
+	//Write message to FIFO
+	data_processed = write(fifo_fd, my_msg, strlen(my_msg));
+	//Print the result written
 	printf("Wrote %d bytes\n",data_processed);
   }
-  
+
+  //Closing the fifo
   close(fifo_fd);
 
   return(0);
