822f02db47dc59a1e1bd2d63e6128b19db2d2780
lucamodica <luca.modica55@gmail.com>
2020-12-14 20:35:16 +0100

    es (queue), done

--- lab_Unix/L9/es_queue/01-init.c
+++ lab_Unix/L9/es_queue/01-init.c
@@ -3,8 +3,6 @@
 #include <unistd.h>
 #include <time.h>
 #include <errno.h>
-#include <string.h>
-#include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
 
@@ -17,7 +15,7 @@
 #define TEST_ERROR    if (errno) {dprintf(STDERR_FILENO, \
 					   "%s:%d: PID=%5d: Error %d (%s)\n",\
 					   __FILE__,\
-					   __LINE__,\
+					   __LINE__,\''
 					   getpid(),\
 					   errno,\
 					   strerror(errno));}
